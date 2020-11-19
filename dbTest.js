7e9429b677578aafa4c9a6434b6a16951e55cb6c
jtreeves <jackson.t.reeves@gmail.com>
2020-11-19 15:46:25 -0500

    [dbTest] Create file for playing with API

---
+++ dbTest.js
@@ -0,0 +1,5 @@
+const db = require('./models')
+
+db.movie.findOrCreate({
+    where: {  }
+})
---
+++ models/movie.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  movie.init({
+    title: {
+        type: DataTypes.TEXT,
+        validate: { notNull: true }
+    },
+    headline: {
+        type: DataTypes.TEXT,
+        validate: { notNull: true }
+    },
+    byline: {
+        type: DataTypes.TEXT,
+        validate: { notNull: true }
+    },
+    date: {
+        type: DataTypes.DATE,
+        validate: { isDate: true }
+    },
+    url: {
+        type: DataTypes.TEXT,
+        validate: { isUrl: true }
+    },
+    sequelize,
+    modelName: 'movie',
+  });
+  return movie;
+};
